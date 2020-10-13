50566db27029895ae9ae523dae3126649bc15ccd
greathumor <silenceluo@icloud.com>
2020-10-13 20:11:47 +0800

    初次提交前台系统，包含课程详情、讲师详情、用户注册和登录

---
+++ api/course.js
@@ -0,0 +1,22 @@
+import request from '~/utils/request'
+export default{
+  getList(searchObj) {
+    return request({
+      url: '/api/edu/course/list',
+      method: 'get',
+      params: searchObj
+    })
+  },
+  getSubjectNestedList() {
+    return request({
+      url: '/api/edu/subject/nested-list',
+      method: 'get'
+    })
+  },
+  getById(id) {
+    return request({
+      url: `/api/edu/course/get/${id}`,
+      method: 'get'
+    })
+  }
+}
---
+++ components/AppHeader.vue
@@ -0,0 +1,132 @@
+<template>
+  <!-- 公共头 -->
+  <header id="header">
+    <section class="container">
+      <h1 id="logo">
+        <a href="#" title="谷粒学院">
+          <img src="~/assets/img/logo.png" width="100%" alt="谷粒学院">
+        </a>
+      </h1>
+      <div class="h-r-nsl">
+        <ul class="nav">
+          <router-link to="/" tag="li" active-class="current" exact>
+            <a>首页</a>
+          </router-link>
+          <router-link to="/course" tag="li" active-class="current">
+            <a>课程</a>
+          </router-link>
+          <router-link to="/teacher" tag="li" active-class="current">
+            <a>名师</a>
+          </router-link>
+          <router-link to="/article" tag="li" active-class="current">
+            <a>文章</a>
+          </router-link>
+          <router-link to="/question" tag="li" active-class="current">
+            <a>问答</a>
+          </router-link>
+        </ul>
+        <!-- / nav -->
+        <ul class="h-r-login">
+          <li v-if="!userInfo" id="no-login">
+            <a href="/login" title="登录">
+              <em class="icon18 login-icon">&nbsp;</em>
+              <span class="vam ml5">登录</span>
+            </a>
+            |
+            <a href="/register" title="注册">
+              <span class="vam ml5">注册</span>
+            </a>
+          </li>
+          <!-- 注意undis将当前节点隐藏了 -->
+          <li v-if="userInfo" id="is-login-one" class="mr10">
+            <a id="headerMsgCountId" href="#" title="消息">
+              <em class="icon18 news-icon">&nbsp;</em>
+            </a>
+            <q class="red-point">&nbsp;</q>
+          </li>
+          <!-- 注意undis将当前节点隐藏了 -->
+          <li v-if="userInfo" id="is-login-two" class="h-r-user">
+            <a href="/ucenter" title>
+              <img
+                :src="userInfo.avatar"
+                :alt="userInfo.nickname"
+                width="30"
+                height="30"
+                class="vam picImg"
+              >
+              <span id="userName" class="vam disIb">{{ userInfo.nickname }}</span>
+            </a>
+            <a href="javascript:void(0)" title="退出" class="ml5" @click="logout()">退出</a>
+          </li>
+          <!-- /未登录显示第1 li；登录后显示第2，3 li -->
+        </ul>
+        <aside class="h-r-search">
+          <form action="#" method="post">
+            <label class="h-r-s-box">
+              <input type="text" placeholder="搜索课程" name="queryCourse.courseName" value>
+              <button type="submit" class="s-btn">
+                <em class="icon18">&nbsp;</em>
+              </button>
+            </label>
+          </form>
+        </aside>
+      </div>
+      <aside class="mw-nav-btn">
+        <div class="mw-nav-icon"/>
+      </aside>
+      <div class="clear"/>
+    </section>
+  </header>
+  <!-- /公共头 -->
+</template>
+
+<script>
+import loginApi from '~/api/login'
+import cookie from 'js-cookie'
+
+export default {
+
+  data() {
+    return {
+      userInfo: null
+    }
+  },
+
+  created() {
+    this.getUserInfo()
+  },
+
+  methods: {
+    getUserInfo() {
+      // 判断客户端是否存在token
+      if (!cookie.get('guli_token')) {
+        return
+      }
+
+      console.log('获取用户信息')
+      // 从客户端获取用户信息:cookie
+      if (cookie.get('guli_user')) {
+        this.userInfo = JSON.parse(cookie.get('guli_user'))
+        return
+      }
+
+      // 从服务器端获取用户信息
+      loginApi.getLoginInfo().then(response => {
+        this.userInfo = response.data.userInfo
+
+        // 将用户信息存入cookie
+        cookie.set('guli_user', this.userInfo, {
+          domain: 'localhost',
+          expires: 1
+        })
+      })
+    },
+
+    logout() {
+      cookie.set('guli_token', '', { domain: 'localhost' })
+      cookie.set('guli_user', '', { domain: 'localhost' })
+      window.location.href = '/'
+    }
+  }
+}
+</script>
